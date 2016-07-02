888aa2c9244168d15bf10fb0f837e31686349c7f
Jeremy Sarda <jcsarda+jeremysarda@gmail.com>
2016-07-01 23:49:35 -0700

    Fixed a test that failed when SVG icons were called from <app-view>

--- tests/angular/app/components/app-view.component.spec.js
+++ tests/angular/app/components/app-view.component.spec.js
@@ -1,8 +1,13 @@
 ngDescribe({
     name: 'Test app-view component',
+    inject: ['$httpBackend'],
     modules: 'app',
     element: '<app-view></app-view>',
     tests: function (deps) {
+
+        beforeEach( () => {
+            deps.$httpBackend.when('GET', /\/img\/icons\/[a-zA-Z0-9-_]*.svg/).respond('');
+        });
 
         it('basic test', () => {
             //
