a198479c1e822e1bcd7d90dc4886e009592088d6
Jeremy Sarda <jcsarda+jeremysarda@gmail.com>
2016-07-02 01:46:20 -0700

    Added waypoint code from previous project.

--- angular/directives/waypoint-target/waypoint-target.directive.js
+++ angular/directives/waypoint-target/waypoint-target.directive.js
@@ -1,16 +1,47 @@
-class WaypointTargetController{
-    constructor(){
+class WaypointTargetController {
+    constructor($log) {
         'ngInject';
-
-        //
+        this.$log = $log;
     }
 }
 
-export function WaypointTargetDirective(){
+WaypointTargetDirective.$inject = ['$log']
+export function WaypointTargetDirective($log) {
     return {
+        restrict: 'EA',
+        scope: {ngModel: '=ngModel'},
         controller: WaypointTargetController,
-        link: function(scope, element, attrs, controllers){
-            //
+        link: function (scope, elem) { //, attrs) {
+
+            var l = $log;
+
+            // $log.debug('--------------------');
+            // $log.debug('Fired WaypointTargetDirective.link()');
+            // $log.debug({'scope': scope});
+            // $log.debug({'elem': elem});
+            // $log.debug({'attrs': attrs});
+            // $log.debug({'scope[direction]': scope[direction]});
+            // $log.debug({'callback': callback});
+            // $log.debug({'$rootScope.bodyClass': $rootScope.bodyClass});
+            // $log.debug('--------------------');
+
+            scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState) { // eslint-disable-line angular/on-watch
+                let fromClasses = angular.isDefined(fromState.data) && angular.isDefined(fromState.data.waypointTargetClass) ? fromState.data.waypointTargetClass : null
+                let toClasses = angular.isDefined(toState.data) && angular.isDefined(toState.data.waypointTargetClass) ? toState.data.waypointTargetClass : null
+
+                l.info({'fromClasses': fromClasses, 'toClasses': toClasses})
+
+                if (fromClasses != toClasses) {
+                    if (fromClasses) {
+                        l.debug({'fromClasses': fromClasses, 'toClasses': toClasses})
+                        elem.removeClass(fromClasses)
+                    }
+                    if (toClasses) {
+                        l.debug({'fromClasses': fromClasses, 'toClasses': toClasses})
+                        elem.addClass(toClasses)
+                    }
+                }
+            })
         }
     }
 }
--- angular/directives/waypoint-trigger/waypoint-trigger.directive.js
+++ angular/directives/waypoint-trigger/waypoint-trigger.directive.js
@@ -1,16 +1,167 @@
-class WaypointTriggerController{
-    constructor(){
+class WaypointTriggerController {
+    constructor($scope, $log, $window) {
         'ngInject';
 
-        //
+        /**
+         * Waypoint slugs parsed into objects and stored by key:
+         * {
+         *     'example.point' : { namespace : 'example', waypoint : 'point' },
+         *     'noNamespace' : { namespace : 'globals', waypoint : 'noNamespace' }
+         * }
+         *
+         * @type Object
+         */
+        this.parsedWaypoints = {};
+        $scope.waypointTriggers = {};
+
+        this.$scope = $scope;
+        this.log = $log;
+        this.d = $window.document;
+    }
+
+    /**
+     * Get a namespace for the waypoint
+     *
+     * @param qualifiedWaypoint
+     * @return String
+     */
+    parseWaypoint(qualifiedWaypoint) {
+        var parts;
+
+        // this.log.debug(['qualifiedWaypoint', qualifiedWaypoint]);
+
+        if (!this.parsedWaypoints[qualifiedWaypoint]) {
+            parts = qualifiedWaypoint.split('.');
+
+            /**
+             * Only trigger event when there are two parts to the direction value.
+             */
+            if (parts.length === 2) {
+                this.parsedWaypoints[qualifiedWaypoint] = {
+                    namespace: parts[0],
+                    waypoint: parts.join('.')
+                };
+            } else {
+                this.log.warn(parts);
+            }
+        }
+        return this.parsedWaypoints[qualifiedWaypoint];
+    }
+
+    /**
+     * Sets all waypoints in the colleciton to false, and sets the indicated waypoint to true.
+     *
+     * @param collection
+     * @param waypoint
+     */
+    setWaypoint(collection, waypoint) {
+        _.each(collection, function (value, waypoint) {
+            collection[waypoint] = false;
+        });
+        collection[waypoint] = true;
+    }
+
+    /**
+     * Clear all waypoints in the same namespace as qualifiedWaypoint,
+     * and set qualifiedWaypoint.
+     *
+     * @param  qualifiedWaypoint
+     * @return void
+     */
+    processWaypoint(qualifiedWaypoint) {
+        var waypoints = this.$scope.waypointTriggers;
+        var data = this.parseWaypoint(qualifiedWaypoint);
+        var namespace = data.namespace;
+        var l = this.log;
+
+        if (!waypoints[namespace]) {
+            waypoints[namespace] = {};
+        }
+
+        // l.debug('---------- processWaypoint -----------');
+        // l.debug(
+        //     {
+        //         'waypoints': waypoints,
+        //         'data': data,
+        //         'namespace': namespace
+        //     }
+        // );
+        // l.debug('--------------------------------------');
+
+        if (data.waypoint) {
+
+            var selectorParts = data.waypoint.split('.'),
+                targetSelector = selectorParts[0],
+                actionClass = selectorParts[1],
+                targets = this.d.getElementsByClassName(targetSelector);
+
+            _.each(targets, function (target, targetIndex) {
+
+                l.debug('--------------- _.each ---------------');
+
+                var mString = namespace,
+                    mRegExp = new RegExp(mString + '-'),
+                    newClassList = [];
+
+                _.each(target.classList, function (cls) {
+                    if (!mRegExp.test(cls)) {
+                        newClassList.push(cls);
+                        l.debug('Keeping... ', cls);
+                    } else {
+                        l.warn('Removing... ', cls);
+                    }
+                });
+
+                newClassList.push(targetSelector + '-' + actionClass);
+                target.className = newClassList.join(' ');
+
+                l.debug(
+                    {
+                        'selectorParts': selectorParts,
+                        'targetSelector': targetSelector,
+                        'actionClass': actionClass,
+                        'targets': targets,
+                        'target': target,
+                        'targetIndex': targetIndex
+                    }
+                );
+
+                l.debug('--------------------------------------');
+
+            });
+
+        } else {
+
+            this.log.warn('No elements found matching selector... Trying the old triggering method.');
+            this.setWaypoint(waypoints[namespace], data.waypoint);
+
+        }
     }
 }
 
-export function WaypointTriggerDirective(){
+WaypointTriggerDirective.$inject = ['$window', 'WaypointService', '$log']
+export function WaypointTriggerDirective($window, WaypointService, $log) {
     return {
         controller: WaypointTriggerController,
-        link: function(scope, element, attrs, controllers){
-            //
+        scope: {
+            up: '@',
+            down: '@',
+            offset: '@',
+            waypointTriggers: '=?waypointTrigger'
+        },
+        link: function (scope, element, attrs, ctrl) {
+
+            // $log.info([scope, element, attrs, ctrl]);
+
+            var callback = angular.bind(ctrl, ctrl.processWaypoint);
+            /*jshint -W031 */
+            new $window.Waypoint({
+                element: element[0],
+                handler: WaypointService.getHandlerSync(scope, callback),
+                offset: scope.offset || 0
+            });
+
+            /*jshint +W031 */
         }
     }
 }
